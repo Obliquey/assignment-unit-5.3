bc2056bd1ed028e817d713e4674d896becb01d07
Anders Boyum <Acboyum@gmail.com>
2023-03-21 13:53:20 -0500

    added addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,19 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
+
+function addToCollection (title, artist, yearPublished) {
+    let record = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished,
+    }
+    collection.push(record);
+    return record;
+}
+addToCollection('The Physics of Fire', 'Becoming The Archetype', '2007');
+addToCollection('Mood Valiant', 'Hiatus Kaiyote', '2021');
+addToCollection('Children Of Fire', 'Oh, Sleeper', '2011');
+
+console.log(collection);
+
