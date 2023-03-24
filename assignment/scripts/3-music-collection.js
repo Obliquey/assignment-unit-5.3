be92d55f30c7d2271613f29357d59db515a22bb4
Anders Boyum <Acboyum@gmail.com>
2023-03-24 10:44:58 -0500

    finished search stretch goals, adding track stretch goals

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,25 +3,53 @@ console.log('***** Music Collection *****')
 // initialize working array for our record store's collection
 let collection = [];
 
+// initializing track array, including track objects within
+let trackArr = [
+    track1 = {
+        name: 'Begin',
+        duration: '2:43'
+    },
+    track2 = {
+        name: 'Top',
+        duration: '5:20',
+    },
+    track3 = {
+        name: 'Start Jam',
+        duration: '3:00'
+    },
+    track4 = {
+        name: 'Groovy',
+        duration: '3:25',
+    },
+    track5 = {
+        name: 'Palace',
+        duration: '10:00',
+    } 
+
+]
+
+
 // create function to add albums to our collection array. have function create object that includes album name, artist's name, and year published properties.
-function addToCollection (title, artist, yearPublished) {
+function addToCollection (title, artist, yearPublished, trackList) {
     // does the variable name here actually matter? When calling from the array later on, it only shows up as array[0], not as record. 
     let record = {
         title: title,
         artist: artist,
         yearPublished: yearPublished,
+        trackList: trackList,
     }
     collection.push(record);
     return record;
 }
 
+
 // add some albums bruh! Some good tunes!
-addToCollection('Transit Blues', 'The Devil Wears Prada', '2016');
-addToCollection('Mood Valiant', 'Hiatus Kaiyote', '2021');
-addToCollection('Children Of Fire', 'Oh, Sleeper', '2011');
-addToCollection('As Blue as Indigo', 'Tigercub', '2021');
-addToCollection('ZII', 'The Devil Wears Prada', '2021');
-addToCollection('Vessel', 'Twenty One Pilots', '2013');
+addToCollection('Transit Blues', 'The Devil Wears Prada', '2016', trackArr);
+addToCollection('Mood Valiant', 'Hiatus Kaiyote', '2021', trackArr);
+addToCollection('Children Of Fire', 'Oh, Sleeper', '2011', trackArr);
+addToCollection('As Blue as Indigo', 'Tigercub', '2021', trackArr);
+addToCollection('ZII', 'The Devil Wears Prada', '2021', trackArr);
+addToCollection('Vessel', 'Twenty One Pilots', '2013', trackArr);
 console.log(collection[0], collection[1], collection[2], collection[3], collection[4], collection[5])
 
 
@@ -58,11 +86,22 @@ function findByArtist (artist) {
 findByArtist('Twenty One Pilots');
 
 
-function search({artist = null, year = null}) {
+// build search function to take parameters artist, year, and trackName as a single object
+function search({artist = null, year = null, trackName = null}) {
+    // init array that will be compiled 
     let foundAlbums = [];
 
-    if (artist == null && year == null) {
+    if (artist == null && year == null && trackName == null) {
         return collection;
+    }
+    else if (trackName !== null) {
+        for (record of collection) {
+            for (trackList of record) {
+                if (trackName == trackList.name) {
+                    
+                }
+            }
+        }
     }
 
     for (let i = 0; i < collection.length; i++) {
